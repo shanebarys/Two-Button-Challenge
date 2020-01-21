73f4063e9275decb705c17ae05aa49508d6b7b3e
Shane Barys <shanejbarys@gmail.com>
2020-01-20 19:49:55 -0500

    Initial Commit

---
+++ Two-Button Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Two-Button Challenge
+//
+//  Created by Shane Barys on 1/20/20.
+//  Copyright © 2020 Shane Barys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
