5635482b0245b65da1b5cbe7ff2a09f8e976ba50
Shane Barys <shanejbarys@gmail.com>
2020-01-20 20:13:50 -0500

    FirstCommit

--- Two-Button Challenge/ViewController.swift
+++ Two-Button Challenge/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var changingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func firstMessageButton(_ sender: UIButton) {
+        changingLabel.text = "You Are Awesome!"
+        changingLabel.textColor = UIColor.blue
+        changingLabel.textAlignment = .left
+    }
+    
+    @IBAction func secondMessageButton(_ sender: Any) {
+        changingLabel.text = "You Are Great!"
+        changingLabel.textColor = .systemRed
+        changingLabel.textAlignment = .right
+    }
 }
 
